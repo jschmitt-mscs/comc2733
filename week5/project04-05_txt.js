a0c82ebfa5b054d8557f326dabe390c74291c19b
jschmitt-mscs <er3709pt@go.minnstate.edu>
2021-10-04 18:58:25 -0500

    finished week 5

---
+++ week5/project04-05_txt.js
@@ -0,0 +1,46 @@
+/*    JavaScript 7th Edition
+      Chapter 4
+      Project 04-05
+
+      Degrees <-> Radians Coverter
+      Author: Joseph Schmitt
+      Date:   10.03.2021
+
+      Filename: project04-05.js
+ */
+
+"use strict";
+
+// Function to convert degrees to radians 
+function degreesToRadians(degrees) {
+   return degrees*Math.PI/180;
+}
+
+// Function to convert radians to degrees
+function radiansToDegrees(radians) {
+   return radians*180/Math.PI;
+}
+
+// Event handler that converts radians to degrees when the input box is changed
+document.getElementById("rValue").onchange = function() {
+   let radians = document.getElementById("rValue").value;
+   console.log("Radians " + radians + "radians");
+   document.getElementById("aValue").value = formatValue3(radiansToDegrees(radian));
+}
+
+// Event handler that converts degrees to radians when the input box is changed
+document.getElementById("aValue").onchange = function() {
+   let degrees = document.getElementById("aValue").value;
+   console.log("Degrees " + degrees +"degrees");
+   document.getElementById("rValue").value = formatValue3(degreesToRadians(degrees));
+}
+
+
+
+
+
+/* ================================================================= */
+ // Function to display a numeric value in the format ##.### 
+ function formatValue3(value) {
+    return value.toFixed(3);
+ }
